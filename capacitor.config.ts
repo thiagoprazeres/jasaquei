d41dc87eefed82768c402b1b92d63c977c9c3325
Thiago Prazeres <thiagoprazeres@gmail.com>
2022-10-09 07:44:53 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.jasaquei.app',
+  appName: 'jasaquei',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
