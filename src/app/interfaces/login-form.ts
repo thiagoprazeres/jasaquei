ae059701170c591d977b8c38eb9b7f2595a9f76e
Thiago Prazeres <thiagoprazeres@gmail.com>
2022-10-11 14:40:21 -0300

    form

---
+++ src/app/interfaces/login-form.ts
@@ -0,0 +1,7 @@
+
+import { FormControl } from '@angular/forms';
+
+export interface LoginForm {
+    login: FormControl<string>;
+    password: FormControl<string>;
+}
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { LoginForm } from 'src/app/interfaces/login-form';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+  loginForm = new FormGroup<LoginForm>({
+    login: new FormControl('', {nonNullable: true}),
+    password: new FormControl('', {nonNullable: true}),
+  });
+
   constructor() { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log('login');
+    console.warn('Login and password', this.loginForm.value);
+    
   }
 
 }
