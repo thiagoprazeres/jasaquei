234aa43e5e775941e1b28f94d44d3b9fd086a202
Thiago Prazeres <thiagoprazeres@gmail.com>
2022-10-17 16:32:03 -0300

    pipe currency BRL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouteReuseStrategy } from '@angular/router';
@@ -12,7 +12,10 @@ import { NgxWebstorageModule } from 'ngx-webstorage';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
+registerLocaleData(localePt, 'pt-BR');
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, NgxWebstorageModule.forRoot(), HttpClientModule, IonicModule.forRoot(), AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
@@ -21,7 +24,7 @@ import { AuthInterceptor } from './interceptors/auth.interceptor';
     // or after 30 seconds (whichever comes first).
     registrationStrategy: 'registerWhenStable:30000'
   })],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }, { provide: LOCALE_ID, useValue: 'pt-BR' }, { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' }],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -6,20 +6,11 @@ import {
 
 export class AuthInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler) {
-
-        // Observação: caso não tenha nada no sessionStorage, que é
-        // o caso deste exemplo, o valor null será o default:
         const auth = JSON.parse(sessionStorage.getItem('auth')) || null;
         if(auth && auth.token && auth.xPessoa) {
-            console.log('AuthorizationInterceptor', auth.token);
-    
-            // Clona a requisição porque não podemos alterar
-            // a instância do HttpHeaders, ela é imutável
             const authReq = req.clone({
                 headers: req.headers.set('Authorization', 'Bearer ' + auth.token).set('x-pessoa', auth.xPessoa)
             });
-    
-            // Devolvemos a nova requisição com o header Authorization:
             return next.handle(authReq);
         } else {
             return next.handle(req);
